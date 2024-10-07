636e8e7850c5b38917648aecc1ab6988957ecee6
Govinda Kumar <113170688+knighthood121@users.noreply.github.com>
2024-10-07 21:28:01 +0530

    Update WorkoutSessions.jsx

--- src/components/WorkoutSessions.jsx
+++ src/components/WorkoutSessions.jsx
@@ -6,44 +6,48 @@ const WorkoutSessions = () => {
       <div className="wrapper">
         <h1>TOP WORKOUT SESSION</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
-          quisquam. Eaque alias, blanditiis quidem maiores illo unde ipsam!
+          Here's a well-rounded workout session that targets all major muscle
+          groups and includes both strength and cardio elements. This session
+          can be done at the gym or at home with minimal equipment.
         </p>
         <img src="/img5.jpg" alt="workout" />
       </div>
       <div className="wrapper">
         <h1>FEATURED BOOTCAMPS</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-          impedit quasi sunt amet rerum accusamus odio eveniet unde?
+          Bootcamp workouts are high-intensity fitness programs that combine
+          elements of strength training, cardiovascular exercise, and functional
+          movements. They are inspired by military training and are designed to
+          push participants to their physical and mental limits
         </p>
         <div className="bootcamps">
           <div>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <h4>Intensity</h4>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-              saepe repellendus nemo sit facere ipsam!
+              Bootcamp workouts are known for their high intensity, involving
+              rapid sequences of exercises with minimal rest.
             </p>
           </div>
           <div>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <h4>Variety</h4>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-              saepe repellendus nemo sit facere ipsam!
+              They include a mix of exercises such as push-ups, squats, lunges and 
+              burpees, often using bodyweight or minimal equipment like resistance 
+              bands and kettlebells
             </p>
           </div>
           <div>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <h4>Group Setting</h4>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-              saepe repellendus nemo sit facere ipsam!
+              These workouts are typically done in groups, fostering a sense of camaraderie
+              and motivation among participants.
             </p>
           </div>
           <div>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+            <h4>Comprehensive Training </h4>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-              saepe repellendus nemo sit facere ipsam!
+              They target all major muscle groups and improve cardiovascular endurance, 
+              strength, flexibility, and coordination.
             </p>
           </div>
         </div>
