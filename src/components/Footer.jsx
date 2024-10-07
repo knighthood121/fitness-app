529364dbaf235fd6570f620187d26a04792e2fd6
Govinda Kumar <113170688+knighthood121@users.noreply.github.com>
2024-10-07 21:29:30 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        Designed And Developed Knighthood....! 
+       All right all right reserved @2024 
     </footer>
   )
 }
